886bbc7fc0308a924fdfd8eba6818bdccff2bb4b
ideltuva <37146812+ideltuva@users.noreply.github.com>
2024-11-01 01:47:30 +0200

    Initialize project

---
+++ src/main/java/com/api/mr/api/controller/HelloWorldController.java
@@ -0,0 +1,20 @@
+package com.api.mr.api.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+@RestController
+@RequestMapping(produces = APPLICATION_JSON_VALUE)
+@Slf4j
+public class HelloWorldController {
+
+    @GetMapping(path = "/hello-world")
+    public ResponseEntity<String> helloWorld() {
+        return ResponseEntity.ok("Hello, World!");
+    }
+}
